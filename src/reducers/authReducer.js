689c4e2c2b644dee4174a5a0fdc67c95ebf6805d
bapoorvasai <bapoorvasai@gmail.com>
2019-08-24 12:29:16 -0600

    Adding Fixed types

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,12 +1,14 @@
+import { SIGN_IN, SIGN_OUT } from '../actions/actionsTypes';
+
 const INITIAL_STATE = {
   isSignedIn: null
 };
 
 export default (prevData = INITIAL_STATE, action) => {
   switch(action.type){
-    case 'SIGN_IN':
+    case SIGN_IN:
       return {...prevData, isSignedIn: true};
-    case 'SIGN_OUT':
+    case SIGN_OUT:
         return {...prevData, isSignedIn: false};
     default:
       return prevData;
